aac9351a30a7682b886ec541d39ff8ca6ba52961
limengyan <2461306030@qq.com>
2020-06-28 23:20:36 +0800

    首次提交

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\zmp_k\Desktop\tiantianshengxian-master\天天生鲜电商项目\lianxi\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
